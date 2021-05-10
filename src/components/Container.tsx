5298d5d97141d9c9276ac8aecf19843d48907e66
huongse7en <huongse7en>
2021-05-10 10:20:15 +0700

    master

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -20,8 +20,7 @@ const Container = styled.div`
   }
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg'),
-      url('/images/left-pancake.svg'), url('/images/right-pancake.svg');
+    background-image: url('/images/arch-${({ theme }) => (theme.isDark ? 'dark' : 'light')}.svg');
     background-repeat: no-repeat;
     background-position: center 420px, 10% 230px, 90% 230px;
     background-size: contain, 266px, 266px;
